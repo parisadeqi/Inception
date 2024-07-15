5eac8ff694871773bb5d5566624e74704282d57a
root <root@inception.myguest.virtualbox.org>
2024-07-15 18:26:29 +0200

    adding new version

--- Makefile
+++ Makefile
@@ -4,13 +4,13 @@ DB_DATA = /home/psadeghi/data/mariadb
 # DB_DATA = /Users/parisasadeqi/data/mariadb
 
 # default target
-all: up
+all: build up
 
 # start the biulding process
 # create the wordpress and mariadb data directories.
 # start the containers in the background and leaves them running
 
-up: build
+up:
 	@mkdir -p $(WP_DATA)
 	@mkdir -p $(DB_DATA)
 	docker-compose -f ./srcs/docker-compose.yml up -d
@@ -31,19 +31,22 @@ start:
 build:
 	docker-compose -f ./srcs/docker-compose.yml build
 
+mariadb:
+	docker exec -it mariadb bash
 # clean the containers
 # stop all running containers and remove them.
 # remove all images, volumes and networks.
 # remove the wordpress and mariadb data directories.
 # the (|| true) is used to ignore the error if there are no containers running to prevent the make command from stopping.
+
 clean:
-	@docker stop $$(docker ps -qa) || true
-	@docker rm $$(docker ps -qa) || true
-	@docker rmi -f $$(docker images -qa) || true
-	@docker volume rm $$(docker volume ls -q) || true
-	@docker network rm $$(docker network ls -q) || true
-	@rm -rf $(WP_DATA) || true
-	@rm -rf $(DB_DATA) || true
+	docker-compose -f ./srcs/docker-compose.yml down -v --rmi all --remove-orphans
+#	docker rmi -f $$(docker images -qa) || true
+#	@docker stop $$(docker ps -qa) || true
+#	@docker rm $$(docker ps -qa) || true
+#	@docker volume rm $$(docker volume ls -q) || true
+#	@sudo rm -rf $(WP_DATA) || true
+#	@sudo rm -rf $(DB_DATA) || true
 
 # clean and start the containers
 re: clean up
--- srcs/requirements/wordpress/conf/wp_conf.sh
+++ srcs/requirements/wordpress/conf/wp_conf.sh
@@ -40,11 +40,23 @@ fi
 # download wordpress core files
 wp core download --allow-root
 # create wp-config.php file with database details
-wp core config --dbhost=mariadb:3306 --dbname="${MYSQL_DB}" --dbuser="${MYSQL_USER}" --dbpass="$MYSQL_PASSWORD" --allow-root
+wp core config --dbhost=mariadb:3306 \
+                --dbname=$MYSQL_DB \
+                --dbuser=$MYSQL_USER \
+                --dbpass=$MYSQL_PASSWORD \
+                --allow-root
 # install wordpress with the given title, admin username, password and email
-wp core install --url="${DOMAIN_NAME}" --title="${WP_TITLE}" --admin_user="${WP_ADMIN_N}" --admin_password="${WP_ADMIN_P}" --admin_email="${WP_ADMIN_E}" --allow-root
+wp core install --url=$DOMAIN_NAME \
+                 --title=$WP_TITLE \
+                 --admin_user=$WP_ADMIN_N \
+                 --admin_password=$WP_ADMIN_P \
+                 --admin_email=$WP_ADMIN_E \
+                 --allow-root
 #create a new user with the given username, email, password and role
-wp user create "${WP_U_NAME}" "${WP_U_EMAIL}" --user_pass="${WP_U_PASS}" --role="${WP_U_ROLE}" --allow-root
+wp user create $WP_U_NAME $WP_U_EMAIL \
+                --user_pass=$WP_U_PASS \
+                --role=$WP_U_ROLE \
+                --allow-root
 
 #---------------------------------------------------php config---------------------------------------------------#
 
@@ -54,4 +66,3 @@ sed -i '36 s@/run/php/php7.4-fpm.sock@9000@' /etc/php/7.4/fpm/pool.d/www.conf
 mkdir -p /run/php
 # start php-fpm service in the foreground to keep the container running
 /usr/sbin/php-fpm7.4 -F
-view raw
